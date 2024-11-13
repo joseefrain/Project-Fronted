12a52649b565a6f902cd1889ab572209696af33a
arleysGatica <gatica.arleys16@gmail.com>
2024-11-12 21:03:49 -0600

    Adding Pagination and filters

--- src/ui/components/Sales/SaleHistory.tsx
+++ src/ui/components/Sales/SaleHistory.tsx
@@ -6,6 +6,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
@@ -26,127 +27,153 @@ import {
 } from '@/components/ui/table';
 import { getFormatedDate } from '@/shared/helpers/transferHelper';
 import { Eye, History, ShoppingBasket } from 'lucide-react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { getSelectedBranchFromLocalStorage } from '../../../shared/helpers/branchHelpers';
+import Pagination from '../../../shared/components/ui/Pagination/Pagination';
 
 export const SaleHistory = () => {
-  const user = useAppSelector((state) => state.auth.signIn.user);
+  const selectedBranchFromLocalStorage = getSelectedBranchFromLocalStorage();
   const salesHistory = useAppSelector((state) => state.sales.branchSales);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
 
   useEffect(() => {
-    store.dispatch(getSalesByBranch(user?.sucursalId?._id ?? '')).unwrap();
+    store
+      .dispatch(getSalesByBranch(selectedBranchFromLocalStorage ?? ''))
+      .unwrap();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
-    <Card className="mt-4 h-[36rem]">
-      <CardHeader>
-        <CardTitle className="flex items-center gap-2">
-          <History />
-          Historial de ventas
-        </CardTitle>
-        <CardDescription>
-          Ver los detalles de las ventas realizadas
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Usuario</TableHead>
-              <TableHead>Fecha</TableHead>
-              <TableHead>Subtotal</TableHead>
-              <TableHead>Descuento</TableHead>
-              <TableHead>Total</TableHead>
-              <TableHead>Productos</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {salesHistory.map((sale, index) => (
-              <TableRow key={index} className="h-[50px]">
-                <TableCell>{sale.userId}</TableCell>
-                <TableCell>{getFormatedDate(new Date())}</TableCell>
-                <TableCell>${sale.subtotal.toFixed(2)}</TableCell>
-                <TableCell>${sale.discount.toFixed(2)}</TableCell>
-                <TableCell>${sale.total.toFixed(2)}</TableCell>
-                <TableCell>
-                  <Dialog>
-                    <DialogTrigger asChild>
-                      <Button variant="outline" size="sm">
-                        <Eye className="w-4 h-4" />
-                        View
-                      </Button>
-                    </DialogTrigger>
-                    <DialogContent className="max-w-3xl rounded-[4px] min-h-[12.5rem]">
-                      <DialogHeader>
-                        <DialogTitle className="flex items-center gap-2 uppercase">
-                          <ShoppingBasket />
-                          Productos
-                        </DialogTitle>
-                      </DialogHeader>
-                      <Table>
-                        <TableHeader>
-                          <TableRow>
-                            <TableHead>Producto</TableHead>
-                            <TableHead className="text-center">
-                              Tipo de cliente
-                            </TableHead>
-                            <TableHead className="text-center">
-                              Cantidad
-                            </TableHead>
-                            <TableHead className="text-center">
-                              Precio
-                            </TableHead>
-                            <TableHead className="text-center">
-                              Descuento
-                            </TableHead>
-                            <TableHead className="text-center">Total</TableHead>
-                          </TableRow>
-                        </TableHeader>
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = salesHistory?.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil((salesHistory ?? []).length / itemsPerPage);
 
-                        <TableBody>
-                          {sale.products.map((item) => (
-                            <TableRow key={item.productId}>
-                              <TableCell>{item.productName}</TableCell>
-                              <TableCell className="text-center">
-                                {item.clientType}
-                              </TableCell>
-                              <TableCell className="text-center">
-                                {item.quantity}
-                              </TableCell>
-                              <TableCell className="text-center">
-                                ${item.price.toFixed(2)}
-                              </TableCell>
-                              <TableCell className="text-center">
-                                {item.discount && item.discount?.amount > 0 ? (
-                                  <span className="text-green-600">
+  return (
+    <div className="flex flex-col w-full">
+      <main className="flex-1 p-4 md:p-6">
+        <Card className="mt-4 h-[36rem]">
+          <CardHeader>
+            <CardTitle className="flex items-center gap-2">
+              <History />
+              Historial de ventas
+            </CardTitle>
+            <CardDescription>
+              Ver los detalles de las ventas realizadas
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Usuario</TableHead>
+                  <TableHead>Fecha</TableHead>
+                  <TableHead>Subtotal</TableHead>
+                  <TableHead>Descuento</TableHead>
+                  <TableHead>Total</TableHead>
+                  <TableHead>Productos</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {currentItems.map((sale, index) => (
+                  <TableRow key={index} className="h-[50px]">
+                    <TableCell>{sale.userId}</TableCell>
+                    <TableCell>{getFormatedDate(new Date())}</TableCell>
+                    <TableCell>${sale.subtotal.toFixed(2)}</TableCell>
+                    <TableCell>${sale.discount.toFixed(2)}</TableCell>
+                    <TableCell>${sale.total.toFixed(2)}</TableCell>
+                    <TableCell>
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button variant="outline" size="sm">
+                            <Eye className="w-4 h-4" />
+                            View
+                          </Button>
+                        </DialogTrigger>
+                        <DialogContent className="max-w-3xl rounded-[4px] min-h-[12.5rem]">
+                          <DialogHeader>
+                            <DialogTitle className="flex items-center gap-2 uppercase">
+                              <ShoppingBasket />
+                              Productos
+                            </DialogTitle>
+                          </DialogHeader>
+                          <Table>
+                            <TableHeader>
+                              <TableRow>
+                                <TableHead>Producto</TableHead>
+                                <TableHead className="text-center">
+                                  Tipo de cliente
+                                </TableHead>
+                                <TableHead className="text-center">
+                                  Cantidad
+                                </TableHead>
+                                <TableHead className="text-center">
+                                  Precio
+                                </TableHead>
+                                <TableHead className="text-center">
+                                  Descuento
+                                </TableHead>
+                                <TableHead className="text-center">
+                                  Total
+                                </TableHead>
+                              </TableRow>
+                            </TableHeader>
+
+                            <TableBody>
+                              {sale.products.map((item) => (
+                                <TableRow key={item.productId}>
+                                  <TableCell>{item.productName}</TableCell>
+                                  <TableCell className="text-center">
+                                    {item.clientType}
+                                  </TableCell>
+                                  <TableCell className="text-center">
+                                    {item.quantity}
+                                  </TableCell>
+                                  <TableCell className="text-center">
+                                    ${item.price.toFixed(2)}
+                                  </TableCell>
+                                  <TableCell className="text-center">
+                                    {item.discount &&
+                                    item.discount?.amount > 0 ? (
+                                      <span className="text-green-600">
+                                        $
+                                        {(
+                                          item.quantity * item.discount.amount
+                                        ).toFixed(2)}{' '}
+                                      </span>
+                                    ) : (
+                                      '-'
+                                    )}
+                                  </TableCell>
+                                  <TableCell className="text-center">
                                     $
                                     {(
-                                      item.quantity * item.discount.amount
-                                    ).toFixed(2)}{' '}
-                                  </span>
-                                ) : (
-                                  '-'
-                                )}
-                              </TableCell>
-                              <TableCell className="text-center">
-                                $
-                                {(
-                                  item.price * item.quantity -
-                                  (item.discount?.amount || 0) * item.quantity
-                                ).toFixed(2)}
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                        </TableBody>
-                      </Table>
-                    </DialogContent>
-                  </Dialog>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </CardContent>
-    </Card>
+                                      item.price * item.quantity -
+                                      (item.discount?.amount || 0) *
+                                        item.quantity
+                                    ).toFixed(2)}
+                                  </TableCell>
+                                </TableRow>
+                              ))}
+                            </TableBody>
+                          </Table>
+                        </DialogContent>
+                      </Dialog>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+          <CardFooter className="flex items-center justify-between">
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={setCurrentPage}
+            />
+          </CardFooter>
+        </Card>
+      </main>
+    </div>
   );
 };
