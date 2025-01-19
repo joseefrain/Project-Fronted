fd2c27df65d106dcab384e24dd872fdd8e06571d
gutherduarte <carlos.duarte@idoagency.com>
2025-01-19 16:52:45 -0600

    Update role access

--- src/ui/components/Table/RoleTable.tsx
+++ src/ui/components/Table/RoleTable.tsx
@@ -18,7 +18,11 @@ import {
 import { Button } from '../../../components/ui/button';
 import { Eye, Pencil, Trash2 } from 'lucide-react';
 import RoleModal from '../Roles/RoleModal';
-import { IRole, IRoleTableProps } from '../../../interfaces/roleInterfaces';
+import {
+  IRole,
+  IRoleAccess,
+  IRoleTableProps,
+} from '../../../interfaces/roleInterfaces';
 import { DialogDescription } from '@radix-ui/react-dialog';
 import { removeRole, updatingRole } from '../../../app/slices/roleSlice';
 import { store } from '../../../app/store';
@@ -26,7 +30,10 @@ import { toast, Toaster } from 'sonner';
 import { useAppSelector } from '../../../app/hooks';
 import { updateRoleAssigned } from '../../../app/slices/login';
 
-const RoleTable = ({ roles }: IRoleTableProps) => {
+const RoleTable = ({
+  roles,
+  access,
+}: IRoleTableProps & { access: IRoleAccess }) => {
   return (
     <>
       <Table className="font-onest">
@@ -38,7 +45,7 @@ const RoleTable = ({ roles }: IRoleTableProps) => {
         </TableHeader>
         <TableBody>
           {roles.map((role) => (
-            <RoleTableRow key={role._id} role={role} />
+            <RoleTableRow key={role._id} role={role} access={access} />
           ))}
         </TableBody>
       </Table>
@@ -49,7 +56,13 @@ const RoleTable = ({ roles }: IRoleTableProps) => {
 
 export default RoleTable;
 
-export const RoleTableRow = ({ role }: { role: IRole }) => {
+export const RoleTableRow = ({
+  role,
+  access,
+}: {
+  role: IRole;
+  access: IRoleAccess;
+}) => {
   const user = useAppSelector((state) => state.auth.signIn.user);
   const isRoleAssignedToUser = user?.roles.find(
     (role) => role._id === role._id
@@ -103,52 +116,56 @@ export const RoleTableRow = ({ role }: { role: IRole }) => {
     <TableRow key={role._id}>
       <TableCell className="text-center">{role.name}</TableCell>
       <TableCell className="flex items-center gap-2">
-        <Dialog open={isEditing} onOpenChange={setIsEditing}>
-          <DialogTrigger asChild>
-            <Button className="w-8 h-8" variant="outline">
-              <Pencil className="w-4 h-4 text-blue-600" />
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="font-onest">
-            <RoleModal
-              onClose={() => setIsEditing(false)}
-              onSave={handleOnUpdateRole}
-              role={role}
-            />
-          </DialogContent>
-        </Dialog>
+        {access.update && (
+          <Dialog open={isEditing} onOpenChange={setIsEditing}>
+            <DialogTrigger asChild>
+              <Button className="w-8 h-8" variant="outline">
+                <Pencil className="w-4 h-4 text-blue-600" />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="font-onest">
+              <RoleModal
+                onClose={() => setIsEditing(false)}
+                onSave={handleOnUpdateRole}
+                role={role}
+              />
+            </DialogContent>
+          </Dialog>
+        )}
 
-        <Dialog open={isDeleting} onOpenChange={setIsDeleting}>
-          <DialogTrigger asChild>
-            <Button className="w-8 h-8" variant="outline">
-              <Trash2 className="w-4 h-4 text-red-600" />
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="flex flex-col font-onest">
-            <DialogHeader className="">
-              <DialogTitle className="font-semibold uppercase">
-                {`Eliminar ${role.name}`}
-              </DialogTitle>
-            </DialogHeader>
-            <DialogDescription>
-              ¿Está seguro de que desea eliminar este rol?
-            </DialogDescription>
-            <DialogFooter>
-              <Button
-                className="w-full text-white bg-gray-400 hover:bg-gray-600"
-                onClick={() => setIsDeleting(false)}
-              >
-                Cancelar
+        {access.delete && (
+          <Dialog open={isDeleting} onOpenChange={setIsDeleting}>
+            <DialogTrigger asChild>
+              <Button className="w-8 h-8" variant="outline">
+                <Trash2 className="w-4 h-4 text-red-600" />
               </Button>
-              <Button
-                className="w-full text-white bg-red-600 hover:bg-red-700"
-                onClick={() => handleOnRemoveRole(role._id)}
-              >
-                Eliminar
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
+            </DialogTrigger>
+            <DialogContent className="flex flex-col font-onest">
+              <DialogHeader className="">
+                <DialogTitle className="font-semibold uppercase">
+                  {`Eliminar ${role.name}`}
+                </DialogTitle>
+              </DialogHeader>
+              <DialogDescription>
+                ¿Está seguro de que desea eliminar este rol?
+              </DialogDescription>
+              <DialogFooter>
+                <Button
+                  className="w-full text-white bg-gray-400 hover:bg-gray-600"
+                  onClick={() => setIsDeleting(false)}
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  className="w-full text-white bg-red-600 hover:bg-red-700"
+                  onClick={() => handleOnRemoveRole(role._id)}
+                >
+                  Eliminar
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        )}
 
         <Dialog open={isViewing} onOpenChange={setIsViewing}>
           <DialogTrigger asChild>
