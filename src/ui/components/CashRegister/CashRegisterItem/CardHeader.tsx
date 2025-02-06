3e5803e045e3f6cf88c35407f6badf7130277428
arleysGatica <gatica.arleys16@gmail.com>
2025-02-05 21:06:39 -0600

    feat: Add pagination and improve cash register history modal UI

--- src/ui/components/CashRegister/CashRegisterItem/CardHeader.tsx
+++ src/ui/components/CashRegister/CashRegisterItem/CardHeader.tsx
@@ -3,6 +3,7 @@ import { CardHeader } from '@/components/ui/card';
 import { IRoleAccess } from '@/interfaces/roleInterfaces';
 import { CashRegisterOpen } from './CashRegisterOpen';
 import { CashRegisterClose } from './CashRegisterClose';
+import { ExternalLink } from 'lucide-react';
 
 interface CardHeaderCashierProps {
   cashRegister: ICajaBrach;
@@ -17,13 +18,18 @@ const CardHeaderCashier = ({
 }: CardHeaderCashierProps) => {
   return (
     <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-      <div
-        onClick={openModalHistory}
-        className="flex items-center justify-center w-10 h-10 rounded-full bg-primary/10"
-      >
-        <span className="text-xl font-bold text-primary">
-          {cashRegister.consecutivo}
-        </span>
+      <div className="flex items-center gap-3">
+        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-primary/10">
+          <span className="text-xl font-bold text-primary">
+            {cashRegister.consecutivo}
+          </span>
+        </div>
+        <div
+          onClick={openModalHistory}
+          className="hover:text-primary hover:cursor-pointer hover:shadow-md"
+        >
+          <ExternalLink />
+        </div>
       </div>
       {access.update &&
         (cashRegister.estado === 'ABIERTA' ? (
