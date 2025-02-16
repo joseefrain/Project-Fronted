27e9fd75760f4ffff20f4f7d8100999acb0560db
arleysgatica <gatica.arleys16@gmail.com>
2025-02-15 19:43:00 -0600

    feat: Adjust ticket formatting in POS ticket generator to improve presentation

--- src/shared/helpers/postTicketGenerator.tsx
+++ src/shared/helpers/postTicketGenerator.tsx
@@ -55,15 +55,15 @@ export const POSTicketGenerator = ({
   const printFrame = useRef<HTMLIFrameElement>(null);
   const typeClient = products[0]?.clientType ?? '';
   const generateTicketText = (): string => {
-    const lineBreak = '---------------------------------------------------';
+    const lineBreak = '------------------------------------------------';
     const space = ' '.repeat(40);
     const center = (text: string, width = 40) =>
       text.padStart((width - text.length) / 2 + text.length).padEnd(width);
 
     let ticketText = `
-${center('ZONA CONEXION', 55)}
-${center('COMOAPA, BOACO', 55)}
-${center('(505) 77041065', 55)}
+${center('ZONA CONEXION', 50)}
+${center('COMOAPA, BOACO', 50)}
+${center('(505) 77041065', 50)}
 ${lineBreak}
 Transaccion: ${transactionId}
 ${lineBreak}
@@ -84,7 +84,7 @@ ${products
     return productLines
       .map((line, index) =>
         index === 0
-          ? `${line.padEnd(30)}${`x${product.quantity}`.padStart(6)}${product.price.toFixed(2).padStart(13)}`
+          ? `${line.padEnd(30)}${`x${product.quantity}`.padStart(6)}${product.price.toFixed(2).padStart(12)}`
           : `${line.padEnd(30)}`
       )
       .join('\n');
@@ -92,14 +92,14 @@ ${products
   .join('\n')}
 
 ${lineBreak}
-EFECTIVO: ${`C${efectivoRecibido}`.padStart(37)}
-CAMBIO: ${`C$${change}`.padStart(41)}
-${totalDiscount ? `DESCUENTO TOTAL: ${`C$${totalDiscount}`.padStart(32)}` : ''}
-TOTAL: ${`C${total}`.padStart(43)}
+EFECTIVO: ${`C${efectivoRecibido}`.padStart(35)}
+CAMBIO: ${`C$${change}`.padStart(38)}
+${totalDiscount ? `DESCUENTO TOTAL: ${`C$${totalDiscount}`.padStart(27)}` : ''}
+TOTAL: ${`C${total}`.padStart(41)}
 ${lineBreak}
 ${space}
 ${space}
-${center('Gracias por su Preferencia', 55)}
+${center('Gracias por su Preferencia', 50)}
 `;
     return ticketText;
   };
